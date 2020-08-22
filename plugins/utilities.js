37b09ae89c5987ee8b70d435e3863ae9aed6f5fc
florian.leica <florian.leica@gmail.com>
2020-08-22 09:57:23 +0200

    Added handler for null values

--- plugins/utilities.js
+++ plugins/utilities.js
@@ -1,4 +1,4 @@
-import { template as _template } from 'lodash'
+import {template as _template} from 'lodash'
 
 /**
  * From an object, converts it in a valid FormData element,
@@ -10,28 +10,28 @@ import { template as _template } from 'lodash'
  * @returns {FormData}
  */
 function formDataFromObject(obj, formDataInstance, prefix) {
-  const formData = formDataInstance || new FormData()
+    const formData = formDataInstance || new FormData()
 
-  for (const entry of Object.entries(obj)) {
-    const key = entry[0]
-    const value = entry[1]
+    for (const entry of Object.entries(obj)) {
+        const key = entry[0]
+        const value = entry[1]
 
-    if (typeof value === 'undefined') {
-      console.info('- formDataFromObject undefinedValue', entry)
+        if (typeof value === 'undefined' || value === null) {
+            console.info('- formDataFromObject undefinedValue', entry)
 
-      continue
+            continue
+        }
+
+        if (value.constructor.name === 'Object') {
+            formDataFromObject(value, formData, key)
+        } else if (value instanceof Array) {
+            value.forEach(entry => formData.append((prefix ? `${prefix}.` : '') + key + '[]', entry))
+        } else {
+            formData.append((prefix ? `${prefix}.` : '') + key, value)
+        }
     }
 
-    if (value.constructor.name === 'Object') {
-      formDataFromObject(value, formData, key)
-    } else if (value instanceof Array) {
-      value.forEach(entry => formData.append((prefix ? `${prefix}.` : '') + key + '[]', entry))
-    } else {
-      formData.append((prefix ? `${prefix}.` : '') + key, value)
-    }
-  }
-
-  return formData
+    return formData
 }
 
 /**
@@ -41,23 +41,23 @@ function formDataFromObject(obj, formDataInstance, prefix) {
  * @param {{}} data
  */
 function compileString(string, data = {}) {
-  /**
-   * @type {TemplateExecutor}
-   */
-  const tmpl = _template(string)
+    /**
+     * @type {TemplateExecutor}
+     */
+    const tmpl = _template(string)
 
-  return tmpl(data)
+    return tmpl(data)
 }
 
 export default (context, inject) => {
-  const utilities = {
-    formDataFromObject,
-    compileString
-  }
+    const utilities = {
+        formDataFromObject,
+        compileString
+    }
 
-  // Inject in Vue, context and store.
-  inject('utilities', utilities)
+    // Inject in Vue, context and store.
+    inject('utilities', utilities)
 
-  // For Nuxt <= 2.12 👇
-  context.$utilities = utilities
+    // For Nuxt <= 2.12 👇
+    context.$utilities = utilities
 }
